58903198c2aa83c0c2775475bc5b399423d96a3a
slash585 <slsh585@gmail.com>
2023-06-12 01:13:42 +0300

    feat: add app icon set

---
+++ SwiftTwitterClone/Controller/FeedController.swift
@@ -0,0 +1,22 @@
+//
+//  FeedController.swift
+//  SwiftTwitterClone
+//
+//  Created by Mehmet Ali Özdemir on 12.06.2023.
+//
+
+import UIKit
+
+class FeedController: UIViewController {
+    //MARK: - Properties
+    
+    //MARK: - Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemMint
+    }
+    
+    //MARK: - Helpers
+}
