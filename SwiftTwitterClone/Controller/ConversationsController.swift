15944e2f23f63fa4f14e1c6b373155f1c9d21c7b
slash585 <slsh585@gmail.com>
2023-06-12 01:48:59 +0300

    feat: setup navigation controllers and bars

--- SwiftTwitterClone/Controller/ConversationsController.swift
+++ SwiftTwitterClone/Controller/ConversationsController.swift
@@ -15,10 +15,14 @@ class ConversationsController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        view.backgroundColor = .systemTeal
+        configureUI()
     }
     
     //MARK: - Helpers
+    
+    func configureUI(){
+        view.backgroundColor = .white
+        navigationItem.title = "Messages"
+    }
     
 }
--- SwiftTwitterClone/Controller/MainTabController.swift
+++ SwiftTwitterClone/Controller/MainTabController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class MainTabController: UITabBarController {
-    
+        
     //MARK: - Properties
     
     //MARK: - Lifecycle
@@ -24,11 +24,24 @@ class MainTabController: UITabBarController {
     func configureViewControllers(){
         
         let feed = FeedController()
+        let nav1 = templateNavigationController(image: UIImage(named: "home_unselected"), rootViewController: feed)
         let explore = ExploreController()
+        let nav2 = templateNavigationController(image: UIImage(named: "search_unselected"), rootViewController: explore)
         let notifications = NotificationsController()
+        let nav3 = templateNavigationController(image: UIImage(named: "like_unselected"), rootViewController: notifications)
         let conversation = ConversationsController()
+        let nav4 = templateNavigationController(image: UIImage(named: "ic_mail_outline_white_2x-1"), rootViewController: conversation)
         
-        viewControllers = [feed, explore, notifications, conversation]
+        
+        
+        viewControllers = [nav1, nav2, nav3, nav4]
+    }
+    
+    func templateNavigationController(image: UIImage?, rootViewController: UIViewController) -> UINavigationController{
+        let nav = UINavigationController(rootViewController: rootViewController)
+        nav.navigationBar.barTintColor = .systemRed
+        nav.tabBarItem.image = image
+        return nav
     }
     
 
