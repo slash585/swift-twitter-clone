4ca8ceb6d1b9a046bb2539e790cad4627033a4c6
slash585 <slsh585@gmail.com>
2023-06-12 00:55:02 +0300

    chore: configure scene delegate

---
+++ SwiftTwitterClone/Controller/MainTabController.swift
@@ -0,0 +1,29 @@
+//
+//  MainTabController.swift
+//  SwiftTwitterClone
+//
+//  Created by Mehmet Ali Özdemir on 12.06.2023.
+//
+
+import UIKit
+
+class MainTabController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemMint
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
