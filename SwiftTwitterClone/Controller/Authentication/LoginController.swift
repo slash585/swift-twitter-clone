40be525b443760d4919cbdff32bfd35d6305723a
slash585 <slsh585@gmail.com>
2023-06-13 23:38:29 +0300

    feat: add twitter logo to login vc

--- SwiftTwitterClone/Controller/Authentication/LoginController.swift
+++ SwiftTwitterClone/Controller/Authentication/LoginController.swift
@@ -10,6 +10,14 @@ import UIKit
 class LoginController: UIViewController {
     //MARK: - Properties
     
+    private let logoImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFit
+        iv.clipsToBounds = true
+        iv.image = #imageLiteral(resourceName: "TwitterLogo")
+        return iv
+    }()
+    
     //MARK: - Lifecycle
 
     override func viewDidLoad() {
@@ -21,5 +29,11 @@ class LoginController: UIViewController {
     
     func configureUI(){
         view.backgroundColor = .twitterBlue
+        navigationController?.navigationBar.barStyle = .black
+        navigationController?.navigationBar.isHidden = true
+        
+        view.addSubview(logoImageView)
+        logoImageView.centerX(inView: view, topAnchor: view.safeAreaLayoutGuide.topAnchor, paddingTop: 0)
+        logoImageView.setDimensions(width: 150, height: 150)
     }
 }
