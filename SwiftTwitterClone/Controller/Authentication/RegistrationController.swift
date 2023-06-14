b4faabd9fb2184e56f1c14226f0a74d8f587075a
slash585 <slsh585@gmail.com>
2023-06-14 23:57:52 +0300

    feat: create RegistrationController

--- SwiftTwitterClone/Controller/Authentication/RegistrationController.swift
+++ SwiftTwitterClone/Controller/Authentication/RegistrationController.swift
@@ -10,6 +10,70 @@ import UIKit
 class RegistrationController: UIViewController {
     //MARK: - Properties
     
+    private let photoPlusButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(UIImage(named: "plus_photo"), for: .normal)
+        button.tintColor = .white
+        button.addTarget(self, action: #selector(handleAddProfilePhoto), for: .touchUpInside)
+        return button
+    }()
+    
+    private lazy var emailContainerView: UIView = {
+        let image = #imageLiteral(resourceName: "ic_mail_outline_white_2x-1")
+        let view = Utilities().inputContainerView(withImage: image, textField: emailTextField)
+        return view
+    }()
+    
+    private lazy var passwordContainerView: UIView = {
+        let image = #imageLiteral(resourceName: "ic_lock_outline_white_2x")
+        let view = Utilities().inputContainerView(withImage: image, textField: passwordTextField)
+        return view
+    }()
+    
+    private lazy var fullNameContainerView: UIView = {
+        let image = #imageLiteral(resourceName: "ic_person_outline_white_2x")
+        let view = Utilities().inputContainerView(withImage: image, textField: fullNameTextField)
+        return view
+    }()
+    
+    private lazy var usernameContainerView: UIView = {
+        let image = #imageLiteral(resourceName: "ic_person_outline_white_2x")
+        let view = Utilities().inputContainerView(withImage: image, textField: usernameTextField)
+        return view
+    }()
+    
+    private let emailTextField: UITextField = {
+        let tf = Utilities().textField(withPlaceholder: "Email")
+        return tf
+    }()
+    
+    private let passwordTextField: UITextField = {
+        let tf = Utilities().textField(withPlaceholder: "Password")
+        return tf
+    }()
+    
+    private let fullNameTextField: UITextField = {
+        let tf = Utilities().textField(withPlaceholder: "Full name")
+        return tf
+    }()
+    
+    private let usernameTextField: UITextField = {
+        let tf = Utilities().textField(withPlaceholder: "Username")
+        return tf
+    }()
+    
+    private let handleRegisterButton: UIButton = {
+        let button = Utilities().loginAndRegisterButton(withButtonTitle: "Sign Up")
+        button.addTarget(self, action: #selector(handleRegister), for: .touchUpInside)
+        return button
+    }()
+    
+    private let alreadyHaveAnAccountButton: UIButton = {
+        let button = Utilities().attributeButton("Already have an account ?", " Login")
+        button.addTarget(self, action: #selector(handleShowSignIn), for: .touchUpInside)
+        return button
+    }()
+    
     //MARK: - Lifecycle
 
     override func viewDidLoad() {
@@ -17,9 +81,42 @@ class RegistrationController: UIViewController {
         configureUI()
     }
     
+    //MARK: - Selectors
+    
+    @objc func handleShowSignIn() {
+        navigationController?.popToRootViewController(animated: true)
+    }
+    
+    @objc func handleAddProfilePhoto() {
+        print("handleAddProfilePhoto")
+    }
+    
+    @objc func handleRegister(){
+        print("handleRegister")
+    }
+    
     //MARK: - Helpers
     
     func configureUI(){
         view.backgroundColor = .twitterBlue
+        navigationController?.navigationBar.barStyle = .black
+        navigationController?.navigationBar.isHidden = true
+        
+        view.addSubview(photoPlusButton)
+        photoPlusButton.centerX(inView: view, topAnchor: view.safeAreaLayoutGuide.topAnchor, paddingTop: 10)
+        photoPlusButton.setDimensions(width: 150, height: 150)
+
+        
+        view.addSubview(alreadyHaveAnAccountButton)
+        alreadyHaveAnAccountButton.anchor(left: view.leftAnchor, bottom: view.safeAreaLayoutGuide.bottomAnchor, right: view.rightAnchor)
+        
+        let stack = UIStackView(arrangedSubviews: [emailContainerView, passwordContainerView, fullNameContainerView, usernameContainerView, handleRegisterButton])
+        stack.axis = .vertical
+        stack.spacing = 20
+        stack.distribution = .fillEqually
+        
+        view.addSubview(stack)
+        stack.anchor(top: photoPlusButton.bottomAnchor, left: view.leftAnchor, right: view.rightAnchor,paddingTop: 32, paddingLeft: 32, paddingRight: 32)
+
     }
 }
