c18884638b78d93f992dda7783828b2a1f73fb18
slash585 <slsh585@gmail.com>
2023-06-18 18:31:05 +0300

    feat: redirect to login screen if user is not logged in

--- SwiftTwitterClone/Controller/MainTabController.swift
+++ SwiftTwitterClone/Controller/MainTabController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Firebase
 
 class MainTabController: UITabBarController {
         
@@ -25,8 +26,32 @@ class MainTabController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        configureViewControllers()
-        configureUI()
+        authenticateUserAndConfigureUI()
+        view.backgroundColor = .twitterBlue
+        //logUserOut()
+    }
+    
+    //MARK: - API
+    
+    func authenticateUserAndConfigureUI(){
+        if Auth.auth().currentUser == nil {
+            DispatchQueue.main.async {
+                let nav = UINavigationController(rootViewController: LoginController())
+                nav.modalPresentationStyle = .fullScreen
+                self.present(nav, animated: true, completion: nil)
+            }
+        } else {
+            configureViewControllers()
+            configureUI()
+        }
+    }
+    
+    func logUserOut(){
+        do {
+            try Auth.auth().signOut()
+        } catch let error {
+            print("DEBUG: Failed to sing out with error \(error.localizedDescription)")
+        }
     }
     
     //MARK: - Selectors
